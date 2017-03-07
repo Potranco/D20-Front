e9bc58405ecbab96344263d6da87139e00d170d4
Chema <jose.rodriguez@scmspain.com>
2017-03-07 15:34:24 +0100

    refactor campaigns & char sub_arrays

--- src/js/classes/Campaigns.js
+++ src/js/classes/Campaigns.js
@@ -2,16 +2,14 @@
 
 function Campaigns(idUser,token,url,callback=null){
    Elements.apply(this,arguments);
-   this.campaigns=[];
 }
 Campaigns.prototype=Object.create(Elements.prototype);
 Campaigns.prototype.constructor=Campaigns;
 
 Campaigns.prototype.add=function(json){
     var aux=json.campaigns.length;
-    
     for (var i=0;i<aux;i++){
-        this.campaigns.push(new Campaign(json.campaigns[i]));
+        this[i]=new Campaign(json.campaigns[i]);
     }
 };
 
--- src/js/classes/Chars.js
+++ src/js/classes/Chars.js
@@ -1,6 +1,5 @@
 function Chars(idUser,token,url,callback=null){
    Elements.apply(this,arguments);
-   this.chars=[];
 }
 Chars.prototype=Object.create(Elements.prototype);
 Chars.prototype.constructor=Chars;
@@ -9,7 +8,7 @@ Chars.prototype.add=function(json){
     var aux=json.chars.length;
     
     for (var i=0;i<aux;i++){
-        this.chars.push(new Char(json.chars[i]));
+        this[i]=new Char(json.chars[i]);
     }
 };
 
